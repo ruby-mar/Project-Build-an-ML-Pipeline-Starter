ac82dee2d282f5df6bd521225e9a23dbddd75e84
Ruby <ema1089@wgu.edu.com>
2024-07-10 20:49:00 -0500

    Pushing Changes Before Release

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,9 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+def test_row_count(data):
+    assert 15000 < data.shape[0] < 1000000
+
+# Setting test for price range
+def test_price_range(data, min_price, max_price):
+    assert data['price'].between(min_price, max_price).all()
